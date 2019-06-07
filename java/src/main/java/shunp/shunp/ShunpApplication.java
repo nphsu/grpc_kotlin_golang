161d3e6cd6d0ad4d75db449f35487e7b64dbeeac
Shunpei Koike <shunpei42ba@gmail.com>
2019-06-07 14:41:44 +0900

    add java project

---
+++ java/src/main/java/shunp/shunp/ShunpApplication.java
@@ -0,0 +1,13 @@
+package shunp.shunp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShunpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShunpApplication.class, args);
+	}
+
+}
