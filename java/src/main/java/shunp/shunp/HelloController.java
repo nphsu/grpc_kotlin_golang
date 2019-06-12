ea68fa480a5bfa774181a03e86358737eb4d40d4
Shunpei Koike <shunpei42ba@gmail.com>
2019-06-12 15:35:41 +0900

    success grpc on docker

--- java/src/main/java/shunp/shunp/HelloController.java
+++ java/src/main/java/shunp/shunp/HelloController.java
@@ -2,15 +2,15 @@ package shunp.shunp;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.examples.helloworld.Greeter;
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
@@ -19,11 +19,17 @@ public class HelloController {
 
     private GreeterGrpc.GreeterBlockingStub blockingStub;
 
+    @Value("${grpcserver:localhost}")
+    private String host;
+
+    @Value("${grpcserver.port:28081")
+    private Integer port;
+
     @PostConstruct
     private void init() {
+        String ip = this.findIpAddr().orElseThrow(RuntimeException::new);
         ManagedChannel managedChannel = ManagedChannelBuilder
-                .forAddress("localhost", 30000).usePlaintext().build();
-
+               .forAddress(ip, port).usePlaintext().build();
         blockingStub = GreeterGrpc.newBlockingStub(managedChannel);
     }
 
@@ -33,5 +39,21 @@ public class HelloController {
         HelloReply reply = blockingStub.sayHello(request);
         System.out.println(reply);
         return HttpStatus.OK;
+    }
+
+    @GetMapping("/shunp")
+    public String shunp() {
+        System.out.println(host);
+        return "shunp";
+    }
+
+    private Optional<String> findIpAddr() {
+        try {
+            java.net.InetAddress inetAddress = java.net.InetAddress.getByName(host);
+            return Optional.of(inetAddress.getHostAddress());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
     }
 }
